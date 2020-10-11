1418fc50e3f9f586206ab6bd8bd3c60ea3b5507d
Jordan Miracle <jordanmiracle@protonmail.com>
2020-10-11 04:26:28 -0700

    Initial commot

---
+++ todoapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todoapp/signupuser.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                    return render(request, 'todoapp/signupuser.html', {'form':UserCreationForm(), 'error':'That user name has already been taken. Please choose a different name.'})
+        else:
+            return render(request, 'todoapp/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match.'})
+
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todoapp/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todoapp/loginuser.html', {'form':AuthenticationForm(), 'error':'Username and password did not match.'})
+            #Tell the user the passwords did not match.
+
+def currenttodos(request):
+    return render(request, 'todoapp/current.html')
