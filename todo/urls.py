84ae762844d0cf629fb46a8ba22bba5097c8c523
Jordan Miracle <jordanmiracle@protonmail.com>
2020-10-12 18:46:18 -0700

    Added logout and login urls , as well as home.

--- todo/urls.py
+++ todo/urls.py
@@ -22,8 +22,12 @@ urlpatterns = [
 
 # Auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
 
 # Todos
+    path('', views.home, name='home'),
     path('current/', views.currenttodos, name='currenttodos'),
+
 
 ]
