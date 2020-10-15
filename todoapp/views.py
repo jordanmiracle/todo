efe77712194f17b1ac501b85aedf7aa9b45c49ab
Jordan Miracle <jordanmiracle@protonmail.com>
2020-10-14 19:31:37 -0700

    Added views for completed and for actually viewing my todo list

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
     return render(request, 'todoapp/home.html')
@@ -63,3 +65,27 @@ def createtodo(request):
 def currenttodo(request):
     todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'todoapp/current.html', {'todos':todos})
+
+@login_required
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'todoapp/current.html', {'todos':todos})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todoapp/viewtodo.html', {'todo':todo, 'form':form, 'error':'Bad Info'})
+
+
+@login_required
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
