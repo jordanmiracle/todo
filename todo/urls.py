d4eaaf443dcec81ff829e11d944662f2200d00f0
Jordan Miracle <jordanmiracle@protonmail.com>
2020-10-18 19:11:18 -0700

    removed duplicate path

--- todo/urls.py
+++ todo/urls.py
@@ -29,7 +29,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.createtodo, name='createtodo'),
     path('current/', views.currenttodos, name='currenttodos'),
-    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
     path('completed/', views.completedtodos, name='completedtodos'),
     path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
 
