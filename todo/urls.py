9db6deeb1c64ae979632443efee94ef7ebc6fbe4
Jordan Miracle <jordanmiracle@protonmail.com>
2020-10-19 04:11:19 -0700

    added url for delete.html

--- todo/urls.py
+++ todo/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('completed/', views.completetodo, name='completetodo'),
     path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
     path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
-    # path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 
 
 ]
